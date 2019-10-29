10243626ab2ea3c89514cc0ab0c581fe8230e2dd
nggrekkodev <nggrekkodev@gmail.com>
2019-10-29 16:13:26 +0100

    Init

---
+++ app.js
@@ -0,0 +1,139 @@
+// BUDGET CONTROLLER
+var budgetController = (function() {
+  var Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  };
+
+  return {
+    addItem: function(type, desc, val) {
+      var newItem, ID;
+
+      // Create new ID
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      // Create new item based on 'exp' or 'inc'
+      if (type === 'exp') newItem = new Expense(ID, desc, val);
+      else if (type === 'inc') newItem = new Income(ID, desc, val);
+
+      // Push item to data structure
+      data.allItems[type].push(newItem);
+      data.totals[type]++;
+
+      return newItem;
+    },
+    testing: function() {
+      console.log(data);
+    }
+  };
+})();
+
+// UI CONTROLLER
+var UIController = (function() {
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputButton: '.add__btn',
+    incomeContainer: '.income__list',
+    expenseContainer: '.expenses__list'
+  };
+
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+    addListItem: function(obj, type) {
+      var html, newHtml, element;
+
+      // Create HTML string with placeholder text
+      if (type === 'inc') {
+        element = DOMstrings.incomeContainer;
+        html =
+          '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      } else if (type === 'exp') {
+        element = DOMstrings.expenseContainer;
+        html =
+          '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div> </div></div>';
+      }
+
+      // Replace the placeholder text with some actual data
+      newHtml = html.replace('%id%', obj.id);
+      newHtml = newHtml.replace('%description%', obj.description);
+      newHtml = newHtml.replace('%value%', obj.value);
+
+      // Insert the HTML
+      document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+    },
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  };
+})();
+
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl) {
+  var setupEventListeners = function() {
+    var DOM = UICtrl.getDOMstrings();
+
+    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+
+  var ctrlAddItem = function() {
+    var input, newItem;
+
+    // 1) Get the filed input data
+    input = UIController.getInput();
+
+    // 2) Add the item to the budget controller
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
+    // 3) Add the item to the UI
+    UICtrl.addListItem(newItem, input.type);
+
+    // 4) Calculate the budget
+    // 5) Display the budget on the UI
+  };
+
+  return {
+    init: function() {
+      console.log('App has started');
+      setupEventListeners();
+    }
+  };
+})(budgetController, UIController);
+
+// START THE APP
+controller.init();
